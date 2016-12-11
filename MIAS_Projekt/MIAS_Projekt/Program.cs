4140cef4d9ce233d2ea82f9549afeac6eda5f18f
maly93 <mat.byra@gmail.com>
2016-12-11 15:30:29 +0100

    test

--- MIAS_Projekt/MIAS_Projekt/Program.cs
+++ MIAS_Projekt/MIAS_Projekt/Program.cs
@@ -13,7 +13,7 @@ namespace MIAS_Projekt
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        {//test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
