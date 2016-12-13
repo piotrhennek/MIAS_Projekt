4aa4593c6308539226f9eba3342988f15a3fb704
maly93 <mat.byra@gmail.com>
2016-12-13 22:15:32 +0100

    poprawny connection string sql dla ef w test

--- MIAS_Projekt/MIAS_Test/MIAST_Logic_EntityFramework.cs
+++ MIAS_Projekt/MIAS_Test/MIAST_Logic_EntityFramework.cs
@@ -1,17 +1,24 @@
 ﻿using MIAS_Logic.EntityFramework;
 using MIAS_Logic.ViciCoolstorage;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity;
+
 namespace MIAS_Test
 {
     [TestClass]
     public class MIAST_Logic_EntityFramework
     {
-        private const string sqlConnectionString = "Initial Catalog=mias;Data Source=localhost;User ID=test;PWD=test;";
+        private const string sqlConnectionString = @"Data Source =localhost; Initial Catalog = mias; Persist Security Info=True;User ID =test; Password=test";
         private const string selectCommad = "select country_id from countries";
         [TestMethod]
         public void SqlEFTest()
         {
-            var efContext = new MIASDbContext(sqlConnectionString);
+
+            //var efContext = new MIASDbContext(sqlConnectionString);
+            using(DbContext context = new MIASDbContext(sqlConnectionString))
+            {
+                 var test =context.Database.SqlQuery<object>("select country_name, country_id from countries");
+            }
         }
 
     }
